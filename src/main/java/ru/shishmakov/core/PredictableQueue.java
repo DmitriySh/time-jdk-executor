540a5337e8d89cae315812f92501cceb5fb112b8
DmitriySh <dashishmakov@gmail.com>
2017-08-18 10:54:12 +0300

    Change ReentrantLock to AtomicBoolean for lock

--- src/main/java/ru/shishmakov/core/PredictableQueue.java
+++ src/main/java/ru/shishmakov/core/PredictableQueue.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 import java.util.Optional;
 import java.util.PriorityQueue;
 import java.util.Queue;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Predicate;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -17,12 +17,12 @@ public class PredictableQueue<E extends Comparable<E>> {
 
     private final Queue<E> queue;
     private final Predicate<E> predicate;
-    private final ReentrantLock lock;
+    private final AtomicBoolean lock;
 
     public PredictableQueue(int capacity, Predicate<E> retrieveRule) {
         this.queue = new PriorityQueue<>(capacity);
         this.predicate = retrieveRule;
-        this.lock = new ReentrantLock();
+        this.lock = new AtomicBoolean(false);
     }
 
     public PredictableQueue(Predicate<E> retrieveRule) {
@@ -30,37 +30,34 @@ public class PredictableQueue<E extends Comparable<E>> {
     }
 
     public Optional<E> poll() {
-        final ReentrantLock lock = this.lock;
-        lock.lock();
+        while (!lock.compareAndSet(false, true)) {/*nothing to do*/}
         try {
             E item = queue.peek();
             if (predicate.test(item)) return Optional.ofNullable(queue.poll());
             else return Optional.empty();
         } finally {
-            lock.unlock();
+            lock.set(false);
         }
     }
 
     public boolean offer(E e) {
         checkNotNull(e, "item is null");
-        final ReentrantLock lock = this.lock;
-        lock.lock();
+        while (!lock.compareAndSet(false, true)) {/*nothing to do*/}
         try {
             return queue.offer(e);
         } finally {
-            lock.unlock();
+            lock.set(false);
         }
     }
 
     public boolean addAll(Collection<? extends E> transfer) {
         checkNotNull(transfer, "collection is null");
         if (transfer.isEmpty()) return false;
-        final ReentrantLock lock = this.lock;
-        lock.lock();
+        while (!lock.compareAndSet(false, true)) {/*nothing to do*/}
         try {
             return queue.addAll(transfer);
         } finally {
-            lock.unlock();
+            lock.set(false);
         }
     }
 
@@ -75,8 +72,7 @@ public class PredictableQueue<E extends Comparable<E>> {
 
     public int drainTo(Collection<? super E> bag, int maxElements) {
         if (maxElements <= 0) return 0;
-        final ReentrantLock lock = this.lock;
-        lock.lock();
+        while (!lock.compareAndSet(false, true)) {/*nothing to do*/}
         try {
             int quantity = 0;
             while (maxElements-- > 0 && !queue.isEmpty()) {
@@ -88,7 +84,7 @@ public class PredictableQueue<E extends Comparable<E>> {
             }
             return quantity;
         } finally {
-            lock.unlock();
+            lock.set(false);
         }
     }
 
